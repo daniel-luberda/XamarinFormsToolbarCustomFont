7a73c97a0f6339148a7054c8e147c7504d1f75a9
daniel <daniel@daniel.com>
2016-09-05 17:21:44 +0200

    Added Android AppCompat support. Fixed #1 #2

--- ToolbarCustomFont.Droid.AppCompat/MainActivity.cs
+++ ToolbarCustomFont.Droid.AppCompat/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Util;
 using Android.Views;
 using Java.Lang.Reflect;
 using Java.Lang;
+using System;
+using Android.Graphics;
 
 namespace ToolbarCustomFont.Droid.AppCompat
 {
@@ -19,48 +21,159 @@ namespace ToolbarCustomFont.Droid.AppCompat
 
             base.OnCreate (savedInstanceState);
 
-            using (var currentInflaterCopy = LayoutInflater.CloneInContext (BaseContext)) 
-            {
-                //Android.Support.V4.View.LayoutInflaterCompat.SetFactory(currentInflaterCopy, new CustomLayoutInflaterFactory());
-                SetFactory(currentInflaterCopy, new CustomLayoutInflaterFactory());
-            }
-
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
             LoadApplication(new App());
         }
 
-        private static void SetFactory(LayoutInflater layoutInflater, Android.Support.V4.View.ILayoutInflaterFactory factory)
-        {
-            layoutInflater.Factory2 = (factory != null ? new FactoryWrapper2(factory) : null);
-        }
-    }
+		static Class ActionMenuItemViewClass = null;
+		static Constructor ActionMenuItemViewConstructor = null;
 
-    public class FactoryWrapper2 : FactoryWrapper, LayoutInflater.IFactory2
-    {
-        public FactoryWrapper2(Android.Support.V4.View.ILayoutInflaterFactory delegateFactory)
-            : base(delegateFactory)
-        {}
+		static Typeface typeface = null;
+		public static Typeface Typeface
+		{
+			get
+			{
+				if (typeface == null)
+					typeface = Typeface.CreateFromAsset(Xamarin.Forms.Forms.Context.ApplicationContext.Assets, "Fonts/fontawesome.ttf");
 
-        public View OnCreateView(View parent, string name, Context context, IAttributeSet attrs)
-        {
-            return DelegateFactory.OnCreateView(parent, name, context, attrs);
-        }
-    }
+				return typeface;
+			}
+		}
 
-    public class FactoryWrapper : Java.Lang.Object, LayoutInflater.IFactory
-    {
-        protected readonly Android.Support.V4.View.ILayoutInflaterFactory DelegateFactory;
+		public override View OnCreateView(string name, Context context, IAttributeSet attrs)
+		{
+			if (name.Equals("android.support.v7.view.menu.ActionMenuItemView", StringComparison.InvariantCultureIgnoreCase))
+			{
+				System.Diagnostics.Debug.WriteLine(name);
+				var customLoginIfNeeded = CreateCustomToolbarItem(name, context, attrs);
+				if (customLoginIfNeeded != null)
+					return customLoginIfNeeded;
+			}
 
-        public FactoryWrapper(Android.Support.V4.View.ILayoutInflaterFactory delegateFactory)
-        {
-            this.DelegateFactory = delegateFactory;
-        }
+			return base.OnCreateView(name, context, attrs);
+		}
 
-        public View OnCreateView(string name, Context context, IAttributeSet attrs)
-        {
-            return this.DelegateFactory.OnCreateView(null, name, context, attrs);
-        }
+		public override View OnCreateView(View parent, string name, Context context, IAttributeSet attrs)
+		{
+			if (name.Equals("android.support.v7.view.menu.ActionMenuItemView", StringComparison.InvariantCultureIgnoreCase))
+			{
+				System.Diagnostics.Debug.WriteLine(name);
+				var customLoginIfNeeded = CreateCustomToolbarItem(name, context, attrs);
+				if (customLoginIfNeeded != null)
+					return customLoginIfNeeded;
+			}
+
+			return base.OnCreateView(parent, name, context, attrs);
+		}
+
+		private View CreateCustomToolbarItem(string name, Context context, IAttributeSet attrs)
+		{
+			// android.support.v7.widget.Toolbar
+			// android.support.v7.view.menu.ActionMenuItemView
+			View view = null;
+
+			try
+			{
+				if (ActionMenuItemViewClass == null)
+					ActionMenuItemViewClass = ClassLoader.LoadClass(name);
+			}
+			catch (ClassNotFoundException ex)
+			{
+				return null;
+			}
+
+			if (ActionMenuItemViewClass == null)
+				return null;
+
+			if (ActionMenuItemViewConstructor == null)
+			{
+				try
+				{
+					ActionMenuItemViewConstructor = ActionMenuItemViewClass.GetConstructor(new Class[] {
+							Class.FromType(typeof(Context)),
+								 Class.FromType(typeof(IAttributeSet))
+						});
+				}
+				catch (SecurityException)
+				{
+					return null;
+				}
+				catch (NoSuchMethodException)
+				{
+					return null;
+				}
+			}
+			if (ActionMenuItemViewConstructor == null)
+				return null;
+
+			try
+			{
+				Java.Lang.Object[] args = new Java.Lang.Object[] { context, (Java.Lang.Object)attrs };
+				view = (View)(ActionMenuItemViewConstructor.NewInstance(args));
+			}
+			catch (IllegalArgumentException)
+			{
+				return null;
+			}
+			catch (InstantiationException)
+			{
+				return null;
+			}
+			catch (IllegalAccessException)
+			{
+				return null;
+			}
+			catch (InvocationTargetException)
+			{
+				return null;
+			}
+			if (null == view)
+				return null;
+
+			View v = view;
+
+			new Handler().Post(() =>
+			{
+
+				try
+				{
+					if (v is LinearLayout)
+					{
+						var ll = (LinearLayout)v;
+						for (int i = 0; i < ll.ChildCount; i++)
+						{
+							var button = ll.GetChildAt(i) as Button;
+
+							if (button != null)
+							{
+								var title = button.Text;
+
+								if (!string.IsNullOrEmpty(title) && title.Length == 1)
+								{
+									button.SetTypeface(Typeface, TypefaceStyle.Normal);
+								}
+							}
+						}
+					}
+					else if (v is TextView)
+					{
+						var tv = (TextView)v;
+						string title = tv.Text;
+
+						if (!string.IsNullOrEmpty(title) && title.Length == 1)
+						{
+							tv.SetTypeface(Typeface, TypefaceStyle.Normal);
+						}
+					}
+				}
+				catch (ClassCastException)
+				{
+				}
+			});
+
+			return view;
+		}
     }
 }
 
